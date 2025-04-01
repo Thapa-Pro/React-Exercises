c6d8adb03f3dd1cf8df832894d70833ed1a64783
Thapa-Pro <bachchan.k.t@gmail.com>
2025-04-01 22:11:01 +0200

    Added related files

--- mypokedb/src/App.jsx
+++ mypokedb/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header"
+import Pokedex from "./pages/Pokedex"
+import TeamGenerator from './pages/TeamGenerator';
+import SearchPokemon from './pages/SearchPokemon';
+import { useState } from 'react';
+import pokemons from './assets/pokemons';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [displayPokedex, setDisplayPokedex] = useState(true);
+  const [displayGenerator, setDisplayGenerator] = useState(false);
+  const [displaySearch, setDisplaySearch] = useState(false);
+  const [pokemonList, setPokemonList] = useState(pokemons);
+
+  const handleRouting = (text) => {
+    if (text === 'Pokedex') {
+      setDisplayPokedex(true);
+      setDisplayGenerator(false);
+      setDisplaySearch(false);
+    } else if (text === 'Team Generator') {
+      setDisplayPokedex(false);
+      setDisplayGenerator(true);
+      setDisplaySearch(false);
+    } else if (text === 'Search Pokemon') {
+      setDisplayPokedex(false);
+      setDisplayGenerator(false);
+      setDisplaySearch(true);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className="app">
+      <Header handleRouting={handleRouting} />
+      {
+        displayPokedex
+          ? <Pokedex pokemons={pokemonList} />
+          : displayGenerator
+            ? <TeamGenerator pokemons={pokemonList} />
+            : displaySearch
+              ? <SearchPokemon pokemons={pokemonList} />
+              : ''
+      }
+
+
+      {/* {
+        displayPokedex && <Pokedex pokemons={pokemonList} />
+      }
+      {
+        displayGenerator && <TeamGenerator pokemons={pokemonList} />
+      }
+      {
+        displaySearch && <SearchPokemon pokemons={pokemonList} />
+      } */}
+    </section>
   )
 }
 
---
+++ mypokedb/src/components/Logo.jsx
@@ -0,0 +1,8 @@
+
+function Logo() {
+    return (
+        <h1 className="header__logo">MyPokeDB</h1>
+    )
+}
+
+export default Logo;
